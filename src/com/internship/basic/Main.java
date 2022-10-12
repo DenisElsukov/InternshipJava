0b76e60ab28ccec67561ee1d239fdad52982314e
Denis Elsukov <delsukov@avaya.com>
2022-10-12 08:48:32 +0200

    The initial project for an internship.

---
+++ src/com/internship/basic/Main.java
@@ -0,0 +1,8 @@
+package com.internship.basic;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Dog", 7, false);
+        animal.animalInfo();
+    }
+}
