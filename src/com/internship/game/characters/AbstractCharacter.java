391fe69bf2081f364066735bc8b9bcdffea88a13
Denis Elsukov <delsukov@avaya.com>
2022-10-14 10:54:19 +0200

    CharacterInterface and AbstractCharacter have been added.

---
+++ src/com/internship/game/characters/AbstractCharacter.java
@@ -0,0 +1,79 @@
+package com.internship.game.characters;
+
+public abstract class AbstractCharacter implements CharacterInterface{
+    private String name;
+    private int movement = 10;
+    private double energy = 10;
+    private String race;
+    private String gender;
+    private double agility = 10;
+
+    public double getAgility() {
+        return agility;
+    }
+
+    public void setAgility(double agility) {
+        this.agility = agility;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMovement() {
+        return movement;
+    }
+
+    public void setMovement(int movement) {
+        this.movement = movement;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        this.energy = energy;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public void actions() {
+        System.out.println("OK, will do it");
+    }
+
+    @Override
+    public void greeting() {
+        System.out.println("Hi!");
+    }
+
+    @Override
+    public void doStep() {
+        System.out.println("OK, stepping");
+    }
+
+    @Override
+    public void attack() {
+        System.out.println("OK, attacking");
+    }
+
+}
