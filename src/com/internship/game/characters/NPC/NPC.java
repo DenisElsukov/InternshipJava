2beaac88fae25a43f7b4e3859fc0f45777d35457
clochard99 <egorushkina.katya@yandex.ru>
2022-10-25 00:50:10 +0200

    UpdateEnums

--- src/com/internship/game/characters/NPC/NPC.java
+++ src/com/internship/game/characters/NPC/NPC.java
@@ -1,13 +1,18 @@
 package com.internship.game.characters.NPC;
 
 public enum NPC {
-    CERBERUS("Roarrr"),HERMIT ("God bless you!"), GREMLIN("Very well, my friend"), DWARF ("Yep"); //ребят, у кого есть другие идеи/варианты, добавляйте)
-    private String sounds;
-    NPC (String sounds) {
-        this.sounds = sounds;
+    CERBERUS("Roarrr", "*echo*"),HERMIT ("God bless you!", "You shall not murder"), GREMLIN("Very well, my friend", "Excuse me"), DWARF ("Yep", "Such a day"), ELF ("I just like to smile", "They say it's going to rain tonight");
+    private String speech1;
+    private String speech2;
+    NPC (String speech1, String speech2) {
+        this.speech1 = speech1;
+        this.speech2 = speech2;
     }
-    public String getSounds() {
-        return sounds;
+    public String getSpeech1() {
+        return speech1;
+    }
+    public String getSpeech2() {
+        return speech2;
     }
 
 }
