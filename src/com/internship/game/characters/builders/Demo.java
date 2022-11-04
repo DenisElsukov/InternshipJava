dae50b500d2e266daa6ad3909b882b5d94759bdf
clochard99 <egorushkina.katya@yandex.ru>
2022-11-05 01:51:27 +0200

    Adding Scanner and removing String

--- src/com/internship/game/characters/builders/Demo.java
+++ src/com/internship/game/characters/builders/Demo.java
@@ -3,41 +3,51 @@ import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Director director = new Director();
         CharactersBuilder builder = new CharactersBuilder();
+
         // Rogue
         director.constructRogue(builder);
         Character character =  builder.getResult();
         System.out.println(character.getType());
         CharacterType characterType = CharacterType.ROGUE;
-        System.out.println(characterType.getGreeting());
-        director.characterEditor();
         CharactersManualBuilder manual = new CharactersManualBuilder();
         director.constructRogue(manual);
         Manual characterManual = manual.getResult();
         System.out.println("Info: \n" + characterManual.print());
+        director.characterEditor();
+        System.out.println(characterType.getGreeting());
+        System.out.println("Do you want to select another character?");
+        String selection = sc.nextLine();
+
         // Wizard
         director.constructWizard(builder);
         Character character1 = builder.getResult();
         System.out.println(character1.getType());
         CharacterType characterType1 = CharacterType.WIZARD;
-        System.out.println(characterType1.getGreeting());
-        director.characterEditor();
         CharactersManualBuilder manual1 = new CharactersManualBuilder();
         director.constructWizard(manual1);
         Manual characterManual1 = manual1.getResult();
         System.out.printf("Info: \n" + characterManual1.print());
+        director.characterEditor();
+        System.out.println(characterType1.getGreeting());
+        System.out.println("Do you want to select another character?");
+        String selection1 = sc.nextLine();
+
         // Warrior
         director.constructWarrior(builder);
         Character character2 = builder.getResult();
         System.out.println(character2.getType());
         CharacterType characterType2 = CharacterType.WARRIOR;
-        System.out.println(characterType2.getGreeting());
-        director.characterEditor();
         CharactersManualBuilder manual2 = new CharactersManualBuilder();
         director.constructWarrior(manual2);
         Manual characterManual2 = manual2.getResult();
         System.out.println("Info: \n" + characterManual2.print());
+        director.characterEditor();
+        System.out.println(characterType2.getGreeting());
+        System.out.println("Do you want to select another character?");
+        String selection2 = sc.nextLine();
     }
 }
 
