5f28ebcb2efb224b076788f8f824bce88a14f06b
Valentine <Vakrokodil@gmail.com>
2022-11-22 19:30:12 +0100

    Add monsters in game

--- src/com/internship/game/characters/NPC/NPC.java
+++ src/com/internship/game/characters/NPC/NPC.java
@@ -1,7 +1,7 @@
 package com.internship.game.characters.NPC;
 
 public enum NPC {
-    CERBERUS("Roarrr", "*echo*"),HERMIT ("God bless you!", "You shall not murder"), GREMLIN("Very well, my friend", "Excuse me"), DWARF ("Yep", "Such a day"), ELF ("I just like to smile", "They say it's going to rain tonight");
+    CERBERUS("Roarrr", "*echo*"), HERMIT ("God bless you!", "You shall not murder"), GREMLIN("Very well, my friend", "Excuse me"), DWARF ("Yep", "Such a day"), ELF ("I just like to smile", "They say it's going to rain tonight");
     private String speech1;
     private String speech2;
     NPC (String speech1, String speech2) {
